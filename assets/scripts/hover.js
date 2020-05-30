b9ca17c151a0a1dd4864c7c08ac5a1254b92d812
AtesSuToprak <hakan@re-coded.com>
2020-05-30 19:32:12 +0300

    Addition of mouse hover tutorial and the message

---
+++ assets/scripts/hover.js
@@ -0,0 +1,19 @@
+////////////////////Following function is only for the first project/////////////////////
+function hoverImages1(){
+	var projectImages = document.getElementById("first-img");
+	projectImages.setAttribute("src","assets/images/google-project.gif")
+}
+function hoverImages1Out(){
+    var projectImages = document.getElementById("first-img");
+	projectImages.setAttribute("src","assets/images/Hakan Yıldız_GoogleMaps5daysProject.png")
+}
+
+////////////////////Following function is only for the third project/////////////////////
+function hoverImages3(){
+	var projectImages = document.getElementById("third-img");
+	projectImages.setAttribute("src","assets/images/Team-A.gif")
+}
+function hoverImages3Out(){
+    var projectImages = document.getElementById("third-img");
+	projectImages.setAttribute("src","assets/images/team-a-project.png")
+}
